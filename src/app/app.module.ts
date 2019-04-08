878f79d029feef063727433ba9339d43f513abae
kohichi horii and Victoria Martinez <kohichi@>
2019-04-08 13:49:07 -0700

    Add sketch img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 // import {Howl, Howler} from 'howler';
 import { AppComponent } from './app.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashPageComponent
   ],
   imports: [
     BrowserModule
