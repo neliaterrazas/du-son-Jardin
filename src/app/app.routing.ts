1d99ef642883b08fa389f4bda4448ee2c9af138a
Isaac Hall and kohichi horii <kohichi@>
2019-04-08 16:34:28 -0700

    route pages

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,15 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SplashPageComponent } from './splash-page/splash-page.component';
+import { PaperCanvasComponent } from './paper-canvas/paper-canvas.component';
+import { KeysComponent } from './keys/keys.component';
 
 
 const appRoutes: Routes = [
-  {
-   path: '',
-   component: SplashPageComponent
- },
+  { path: '', redirectTo: '/splash', pathMatch: 'full' },
+  { path: 'splash', component: SplashPageComponent },
+  { path: 'paper', component: PaperCanvasComponent }
+  { path: 'keys', component: KeysComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
