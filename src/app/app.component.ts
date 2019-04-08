1272e6eea96ad36cd89c11d6b346a08f2d009e37
Isaac Hall and Nelia Terrazas <isaac@>
2019-04-08 10:16:09 -0700

    Add initialization of howlerjs with keydown activation of sound

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Howl } from 'howler'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() {
+
+    // var sound = new Howl({
+    //   src: ['http://server8.mp3quran.net/ahmad_huth/001.mp3'],
+    //   html5 :true
+    // });
+    var keyData = {
+      q: {
+        sound: new Howl({
+          src: ['http://server8.mp3quran.net/ahmad_huth/001.mp3']
+        }),
+        // color: '#1abc9c'
+      }
+    }
+    document.onkeydown = function(event) {
+      if(keyData[event.key]) {
+        keyData[event.key].sound.play();
+      }
+    }
+    // sound.play();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// import {Howl, Howler} from 'howler';
 import { AppComponent } from './app.component';
 
 
